f4bc61ee0bc4dae1fae45e657475c6a15c21cc32
DragOnMon5 <shogenov.vlad@mail.ru>
2024-03-31 17:24:50 +0300

    added fragment

--- app/src/main/java/com/example/new_clicker/MainActivity.kt
+++ app/src/main/java/com/example/new_clicker/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.new_clicker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.example.new_clicker.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var settingsFragment: Settings
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        settingsFragment = Settings()
+//        binding.button.setOnClickListener { ActivateSettingsFragment() }
+
+        binding.settingsButton.setOnClickListener {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<Settings>(R.id.fragment_container_view)
+            }
+        }
+
     }
+        /* fun ActivateSettingsFragment() {
+
+
+         }*/
+
+
 }
