6155ea68ef61f9f42482b51df058ca284f20740d
godazArt <godazArt@mail.ru>
2024-04-07 16:11:33 +0300

    added funcional
    added funcional for button and display for balance

--- app/src/main/java/com/example/new_clicker/Engine.java
+++ app/src/main/java/com/example/new_clicker/Engine.java
@@ -1,5 +1,15 @@
 package com.example.new_clicker;
 
 public class Engine {
-    double  click_power = 1, click_per_second = 0;
+    double  click_power = 1, click_per_second = 0, total_score = 0, balance = 0, multipler = 1;
+
+    void click_action()
+    {
+        balance += click_power * multipler;
+    }
+
+    double get_balance()
+    {
+        return balance;
+    }
 }
--- app/src/main/java/com/example/new_clicker/MainActivity.kt
+++ app/src/main/java/com/example/new_clicker/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.new_clicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val engine = Engine()
+        val balance_display:TextView = findViewById(R.id.Balance_text)
+        val main_button:ImageButton = findViewById(R.id.mainButton)
+
+        main_button.setOnClickListener{
+            engine.click_action();
+            balance_display.setText(engine._balance.toString())
+        }
     }
 }
