274c39ea3dd56e388e126516ccdb0e32d0e43021
DragOnMon5 <shogenov.vlad@mail.ru>
2024-04-08 19:15:26 +0300

    fixed the mechanism for creating fragments

--- app/src/main/java/com/example/new_clicker/MainActivity.kt
+++ app/src/main/java/com/example/new_clicker/MainActivity.kt
@@ -7,11 +7,14 @@ import com.example.new_clicker.databinding.ActivityMainBinding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.lifecycle.Lifecycle
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     lateinit var settingsFragment: Settings
+    var n = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,17 +23,33 @@ class MainActivity : AppCompatActivity() {
 //        binding.button.setOnClickListener { ActivateSettingsFragment() }
 
         binding.settingsButton.setOnClickListener {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<Settings>(R.id.fragment_container_view)
-            }
+            ActivateSettingsFragment()
+            binding.textView.text = n.toString()
+            n++
+        }
+        //активирует сразу(или добавляет на экран) фрагмент
+        /*supportFragmentManager.commit {
+            add<Settings>(R.id.fragment_container_view)
+        }*/
+    }
+    private fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().setMaxLifecycle(fragment, Lifecycle.State.RESUMED).show(fragment).commit()
+
+    }
+
+    private fun hideFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().setMaxLifecycle(fragment, Lifecycle.State.CREATED).hide(fragment).commit()
+    }
+    fun ActivateSettingsFragment() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+//            add<Settings>(R.id.fragment_container_view)
+            replace<Settings>(R.id.fragment_container_view)
+                //addToBackStack("First tab")
+
         }
 
     }
-        /* fun ActivateSettingsFragment() {
-
-
-         }*/
 
 
 }
--- app/src/main/java/com/example/new_clicker/Settings.kt
+++ app/src/main/java/com/example/new_clicker/Settings.kt
@@ -2,14 +2,23 @@ package com.example.new_clicker
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.commit
+import com.example.new_clicker.databinding.SettingsBinding
 
 class Settings : Fragment() {
-
+    private var binding : SettingsBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = SettingsBinding.inflate(layoutInflater)
+        binding?.soundSwitch?.setOnClickListener{ }
+    }
 
+    override fun onResume() {
+        super.onResume()
     }
 
     override fun onCreateView(
@@ -17,7 +26,24 @@ class Settings : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.settings, container, false)
+        binding = SettingsBinding.inflate(inflater, container, false)
+        binding?.fragmentField?.setOnClickListener{}
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            parentFragmentManager.commit {
+                parentFragmentManager.beginTransaction().remove(this@Settings).commit()
+            }
+        }
+        binding?.containerRoot?.setOnClickListener {
+            activity?.supportFragmentManager?.commit {
+                remove(this@Settings)
+            }
+        }
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 
     companion object {
