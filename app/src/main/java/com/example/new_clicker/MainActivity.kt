cd089b6a1032ae96a55e76be90a8b1153c93f679
DragOnMon5 <shogenov.vlad@mail.ru>
2024-04-18 13:39:29 +0300

    added store fragment

--- app/src/main/java/com/example/new_clicker/MainActivity.kt
+++ app/src/main/java/com/example/new_clicker/MainActivity.kt
@@ -14,12 +14,14 @@ import androidx.lifecycle.Lifecycle
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     lateinit var settingsFragment: Settings
+    lateinit var storeFragment: StoreFragment
     var n = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         settingsFragment = Settings()
+        storeFragment = StoreFragment()
 //        binding.button.setOnClickListener { ActivateSettingsFragment() }
 
         binding.settingsButton.setOnClickListener {
@@ -27,28 +29,41 @@ class MainActivity : AppCompatActivity() {
             binding.textView.text = n.toString()
             n++
         }
+
+        binding.storeButton.setOnClickListener {
+            ActivateStoreFragment()
+        }
         //активирует сразу(или добавляет на экран) фрагмент
         /*supportFragmentManager.commit {
             add<Settings>(R.id.fragment_container_view)
         }*/
     }
+
     private fun showFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().setMaxLifecycle(fragment, Lifecycle.State.RESUMED).show(fragment).commit()
+        supportFragmentManager.beginTransaction().setMaxLifecycle(fragment, Lifecycle.State.RESUMED)
+            .show(fragment).commit()
 
     }
 
     private fun hideFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().setMaxLifecycle(fragment, Lifecycle.State.CREATED).hide(fragment).commit()
+        supportFragmentManager.beginTransaction().setMaxLifecycle(fragment, Lifecycle.State.CREATED)
+            .hide(fragment).commit()
     }
+
     fun ActivateSettingsFragment() {
         supportFragmentManager.commit {
             setReorderingAllowed(true)
 //            add<Settings>(R.id.fragment_container_view)
             replace<Settings>(R.id.fragment_container_view)
-                //addToBackStack("First tab")
-
+            //addToBackStack("First tab")
         }
+    }
 
+    fun ActivateStoreFragment() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<StoreFragment>(R.id.fragment_container_view)
+        }
     }
 
 
